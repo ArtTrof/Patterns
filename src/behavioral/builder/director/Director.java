8bbbeb954e4566dfaea0dc41bbc9733e5b76cef3
Artem Trofymenko <randallsng@gmail.com>
2023-03-22 15:04:43 +0100

    pattern builder done

---
+++ src/behavioral/builder/director/Director.java
@@ -0,0 +1,39 @@
+package behavioral.builder.director;
+
+import behavioral.builder.builders.Builder;
+import behavioral.builder.cars.CarType;
+import behavioral.builder.components.Engine;
+import behavioral.builder.components.GPSNavigator;
+import behavioral.builder.components.Transmission;
+import behavioral.builder.components.TripComputer;
+
+/**
+ * Director defines the order of building steps. It works with a builder object
+ * through common Builder interface. Therefore it may not know what product is
+ * being built.
+ */
+public class Director {
+    public void constructSportCar(Builder builder) {
+        builder.setCarType(CarType.SPORTS_CAR);
+        builder.setSeats(2);
+        builder.setEngine(new Engine(3, 0));
+        builder.setTransmission(Transmission.SEMI_AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGPSNavigator(new GPSNavigator());
+    }
+    public void constructCityCar(Builder builder){
+        builder.setCarType(CarType.CITY_CAR);
+        builder.setSeats(4);
+        builder.setEngine(new Engine(1.2, 0));
+        builder.setTransmission(Transmission.AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGPSNavigator(new GPSNavigator());
+    }
+    public  void constructSUV(Builder builder){
+        builder.setCarType(CarType.SUV);
+        builder.setSeats(4);
+        builder.setEngine(new Engine(2.5, 0));
+        builder.setTransmission(Transmission.MANUAL);
+        builder.setGPSNavigator(new GPSNavigator());
+    }
+}
