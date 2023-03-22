b11cdd253c3b4e3c9128da00fc3549323e8b4920
Artem Trofymenko <randallsng@gmail.com>
2023-03-22 15:12:58 +0100

    pattern factory done

---
+++ src/behavioral/factory/buttons/Button.java
@@ -0,0 +1,8 @@
+package behavioral.factory.buttons;
+/**
+ * Common interface for all buttons.
+ */
+public interface Button {
+    void render();
+    void onClick();
+}
---
+++ src/behavioral/factory/factory/HtmlDialog.java
@@ -0,0 +1,14 @@
+package behavioral.factory.factory;
+
+import behavioral.factory.buttons.Button;
+import behavioral.factory.buttons.HtmlButton;
+
+/**
+ * HTML Dialog will produce HTML buttons.
+ */
+public class HtmlDialog extends Dialog {
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
+}
